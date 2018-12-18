c63dcb114dd81eec1edef081729ec094802e0093
Denisteam <0brein0@mail.ru>
2018-12-18 23:39:57 +0700

    just refactoring

--- src/main/java/archivation/lz77/LZ77.java
+++ src/main/java/archivation/lz77/LZ77.java
@@ -1,12 +1,17 @@
 package archivation.lz77;
 
+import javafx.util.Pair;
 import utils.BitsWriter;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Reader;
 
 public class LZ77 {
-    private byte[] window = new byte[65536];
+    private static final int DICTIONARY_SIZE = 1 << 15;
+    private static final int BUFFER_SIZE = DICTIONARY_SIZE >> 2;
+    private char[] window = new char[DICTIONARY_SIZE + BUFFER_SIZE];
+
     private int pointer = 0;
     private Reader reader;
     private BitsWriter writer;
@@ -16,5 +21,34 @@ public class LZ77 {
         writer = new BitsWriter(out);
     }
 
+    public void compress() throws IOException {
+        int n = reader.read(window, DICTIONARY_SIZE, BUFFER_SIZE);
+        if (n < BUFFER_SIZE) finalizeCompression(n);
+
+        int max = 0;
+        int p = 0;
+        int r;
+        while ((r = reader.read()) != -1) {
+            for (int i = 0; i < DICTIONARY_SIZE; i++) {
+                if (window[i] == window[DICTIONARY_SIZE]) {
+                    int len = 0;
+
+
+                    if (len > max) {
+                        max = len;
+                        p = i;
+                    }
+                }
+            }
+        }
+    }
+
+    private void finalizeCompression(int n) {
+
+    }
+
+    private void shift(int val) {
+
+    }
 
 }
--- src/main/java/utils/StartStepStopCodes.java
+++ src/main/java/utils/StartStepStopCodes.java
@@ -3,12 +3,39 @@ package utils;
 public class StartStepStopCodes {
     private final static int MAX_COUNT = 32;
 
-    public void getCountOfLeadingZeros(int value) {
+
+    /*
+    * @param value encoding number
+    * @return: count of leading zeroes in start step stop prefix
+    * */
+    public static int getCountOfLeadingZeros(int value) {
         int i = 0;
         while (value != 0) {
             value >>>= 1;
             i++;
         }
+
+        i /= 2;
+
+        if (i < 1) {
+            return 0;
+        }
+
+        if (i > 15) {
+            return 14;
+        }
+
+        return i - 1;
+    }
+
+    public static int getCountOfDigitsByPrefixZeroCount(int zeroCount) {
+        if (zeroCount < 0 || zeroCount > 14) throw new IllegalArgumentException(String.valueOf(zeroCount));
+
+        if (zeroCount == 0) return 3;
+
+        if (zeroCount == 14) return 32;
+
+        return 3 + zeroCount * 2;
     }
 
 }
